1c03afa47b223c3c779f96aef67627167a3fc9d8
Student <Student@304-1>
2020-02-18 20:59:48 +0200

    1c

---
+++ Interface/Car/Tank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Car
+{
+    class Tank
+    {
+        uint volume;
+        double fuel;
+        public uint Volume
+        {
+            get => volume;
+            private set => volume = value >= 40 && value <= 120 ? value : throw new Exception("Bad tank volume");
+        }
+        public double Fuel
+        {
+            get => fuel;
+            set
+            {
+                if (value <= 0 )
+                {
+                    fuel = 0;
+                }
+            }
+        }
+    }
+}
