7d124dfa878da65904010afcbe2a880ed0125770
Young Lover <younglover17771@gmail.com>
2020-04-09 16:32:37 +0300

    base

--- BInaryTree/main.cpp
+++ BInaryTree/main.cpp
@@ -19,6 +19,72 @@ class BTree
 		}
 		friend class BTree;
 	}*root;
+private:
+	void insert(int data, Element* root)
+	{
+		if (this->root == nullptr)//Если дерево пустое, то добавляем элемент прямо в корень.
+		{
+			this->root = new Element(data);
+			return;
+		}
+		if (root == nullptr)return;
+		if (data < root->data)
+		{
+			if (root->pLeft == nullptr)root->pLeft = new Element(data);
+			else insert(data, root->pLeft);
+		}
+		else
+		{
+			if (root->pRight == nullptr) root->pRight = new Element(data);
+			else insert(data, root->pRight);
+		}
+	}
+	void erase(int v, Element*& root)
+	{
+		if (root == nullptr)return;
+		if (v < root->data)
+		{
+			erase(v, root->pLeft);
+		}
+		else if (v > root->data)
+		{
+			erase(v, root->pRight);
+		}
+		else
+		{
+			if (root->pLeft == root->pRight)
+			{
+				//delete root;
+				root = nullptr;
+			}
+			else if (size(root->pLeft)>size(root->pRight)) {
+				root->data = maxValue(root->pLeft);
+				erase(maxValue(root->pLeft), root->pLeft);
+			}
+			else
+			{
+				root->data = minValue(root->pRight);
+				erase(minValue(root->pRight), root->pRight);
+			}
+		}
+	}
+	int sum(Element* root)const
+	{
+		if (root == nullptr)return 0;
+		return sum(root->pLeft) + sum(root->pRight) + root->data;
+	}
+	int size(Element* root)const
+	{
+		if (root == nullptr)return 0;
+		return size(root->pLeft) + size(root->pRight) + 1;
+	}
+	void print(Element* root)const
+	{
+		if (root == nullptr)return;
+		print(root->pLeft);
+		std::cout << root->data << "\t";
+		print(root->pRight);
+	}
 public:
 	Element* getRoot()
 	{
@@ -40,25 +106,12 @@ public:
 	{
 		insert(data, this->root);
 	}
-
-	void insert(int data,Element* root)
+	void erase(int v)
 	{
-		if (this->root == nullptr)//Если дерево пустое, то добавляем элемент прямо в корень.
-		{
-			this->root = new Element(data);
-			return;
-		}
-		if (root == nullptr)return;
-		if (data < root->data)
-		{
-			if (root->pLeft == nullptr)root->pLeft = new Element(data);
-		}
-		else
-		{
-			if (root->pRight == nullptr) root->pRight = new Element(data);
-			else insert(data, root->pRight);
-		}
+		return erase(v, this->root);
 	}
+	
+	
 
 	void clear()
 	{
@@ -95,33 +148,58 @@ public:
 
 	}
 
+	int size()const
+	{
+		return size(this->root);
+	}
+	int sum()const
+	{
+		
+		return sum(this->root);
+	}
+	double avg()const
+	{
+		return double(sum(this->root))/size(this->root);
+	}
 	void print()
 	{
 		print(this->root);
 		std::cout << std::endl;
 	}
 
-	void print(Element* root)const
-	{
-		if (root == nullptr)return;
-		print(root->pLeft);
-		std::cout << root->data << "\t";
-		print(root->pRight);
-	}
+	
 };
+
+#define BASE_CHECK
 
 void main() 
 {
+	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
 	int n;
 	std::cout << "Input number of element: "; std::cin >> n;
-
 	BTree T800;
 	for (int i = 0; i < n; i++)
 	{
-		T800.insert(rand(), T800.getRoot());
+		T800.insert(rand() % 100);
 	}
 	T800.print();
-	
-	std::cout << T800.minValue() << std::endl;
-	std::cout << T800.maxValue() << std::endl;
+
+	std::cout << T800.size() << std::endl;
+	std::cout << T800.sum() << std::endl;
+	std::cout << T800.avg() << std::endl;
+	std::cout << "Введите значение удаляемого значения: "; int v; std::cin >> v; std::cout << "\n";
+	T800.erase(v);
+	T800.print();
+#endif // BASE_CHECK
+
+	/*BTree doob;
+	doob.insert(50);
+	doob.insert(25);
+	doob.insert(80);
+	doob.insert(50);
+	doob.insert(50);*/
+
+	//std::cout << T800.minValue() << std::endl;
+	//std::cout << T800.maxValue() << std::endl;
 }
